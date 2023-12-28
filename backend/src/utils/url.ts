5a5c37c581c574291cd902b415ec304ef10fd2eb
tuterwell <46310069+tuterwell@users.noreply.github.com>
2023-12-28 23:38:54 +0800

    Update url.ts of url

--- backend/src/utils/url.ts
+++ backend/src/utils/url.ts
@@ -1,5 +1,3 @@
-export const backendurl =
-  process.env.NODE_ENV === 'production' ? 'https://api.example.com' : 'https://112-1-database-final-jygy.vercel.app/api';
+export const backendurl = 'https://112-1-database-final-jygy.vercel.app/api';
 
-export const frontendUrl =
-  process.env.NODE_ENV === 'production' ? 'https://example.com' : 'https://112-1-database-final.vercel.app';
+export const frontendUrl = 'https://112-1-database-final.vercel.app';
