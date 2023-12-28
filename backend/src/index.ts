47a0f6c46ba102bcd5789bdeed995592a8767426
tuterwell <46310069+tuterwell@users.noreply.github.com>
2023-12-28 23:41:43 +0800

    Update index.ts of cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,8 +25,8 @@ const app = express();
 
 app.use(
   cors({
-    credentials: true,
     origin: 'https://112-1-database-final.vercel.app',
+    credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   })
 );
