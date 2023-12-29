f98f321d7e45a40ebb5705e73f396f78f9ec83e9
tuterwell <46310069+tuterwell@users.noreply.github.com>
2023-12-29 14:31:15 +0800

    Update url.ts

--- backend/src/utils/url.ts
+++ backend/src/utils/url.ts
@@ -1,3 +1,3 @@
-export const backendurl = 'https://112-1-database-final-jygy.vercel.app:8080/api';
+export const backendurl = 'https://112-1-database-final-jygy.vercel.app/api';
 
 export const frontendUrl = 'https://112-1-database-final.vercel.app';
