0758a6102f132782a6c42b0039b93d369047e7a8
tuterwell <46310069+tuterwell@users.noreply.github.com>
2023-12-29 09:32:41 +0800

    Update axios.ts for adding port

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const url = 'https://112-1-database-final-jygy.vercel.app/api';
+export const url = 'https://112-1-database-final-jygy.vercel.app:8080/api';
 
 const instance = axios.create({
   baseURL: url,
